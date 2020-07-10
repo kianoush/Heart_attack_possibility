0b71cb7a3b7a39a543eea48e74f984509a352f26
k.karimipor@gmail.com <Kian1212>
2020-07-09 19:05:26 -0700

    First Step

--- Data_set_on_Heart_attack_possibility.py
+++ Data_set_on_Heart_attack_possibility.py
@@ -9,14 +9,30 @@ Attribute Information
 3) chest pain type (4 values)
 4) resting blood pressure
 5) serum cholestoral in mg/dl
-6)fasting blood sugar > 120 mg/dl
+6)fasting blood sugar > 120 mg/dl(values 0,1)
 7) resting electrocardiographic results (values 0,1,2)
 8) maximum heart rate achieved
-9) exercise induced angina
+9) exercise induced angina (values 0,1)
 10) oldpeak = ST depression induced by exercise relative to rest
-11)the slope of the peak exercise ST segment
-12) number of major vessels (0-3) colored by flourosopy
+11) the slope of the peak exercise ST segment(values 0,1,2)
+12) number of major vessels (0-3) colored by flourosopy (values 0,1,2,3,4)
 13) thal: 0 = normal; 1 = fixed defect; 2 = reversable defect
-14) target: 0= less chance of heart attack 1= more chance of heart attack
+14) target: 0= less chance of heart attack 1= more chance of heart attack ---- label
 
 """
+
+import torch
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+print(os.listdir())
+
+raw_data = pd.read_csv('datasets_heart.csv')
+
+print(np.unique(raw_data.ca))
+
+
+print('END!')
