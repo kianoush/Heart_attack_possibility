e0d33c499415f79fdc8d381b895a024e4904ca6a
k.karimipor@gmail.com <Kian1212>
2020-07-15 19:43:21 -0700

    Data cleaning

--- Data_set_on_Heart_attack_possibility.py
+++ Data_set_on_Heart_attack_possibility.py
@@ -84,14 +84,15 @@ raw_data.oldpeak =Data_cleaning.oldpeak(raw_data.oldpeak)
 print(raw_data.info())
 
 
-df = raw_data.iloc[:, :13]
+df = raw_data.iloc[:, :9]
 label = raw_data.iloc[:, 13]
+print(df.shape)
 
 """
 Split data
 """
-x_train, x_test, y_train, y_test = train_test_split(df, label, test_size=0.2, shuffle=True)
-x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.1, shuffle=True)
+x_train, x_test, y_train, y_test = train_test_split(df, label, test_size=0.2, shuffle=True,random_state=12)
+x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.1, shuffle=True,random_state=12)
 
 
 x_train = torch.tensor(x_train.values).float()
@@ -101,60 +102,77 @@ x_valid = torch.tensor(x_valid.values).float()
 y_train = torch.tensor(y_train.values).float()
 y_test = torch.tensor(y_test.values).float()
 y_valid = torch.tensor(y_valid.values).float()
+p=0.001
+d=[]
+for i in range(5, 20):
+    for j in range(5, 15):
+        for k in range (100):
 
-Sample_num = x_train.shape[1]
-Class_num = 2
-Hiddenl = 7
-#h1 =5
-
-"""
-Model
-"""
-
-model = torch.nn.Sequential(torch.nn.Linear(Sample_num, Hiddenl),
-                            torch.nn.ReLU(),
-                            torch.nn.Linear(Hiddenl, Class_num),
-                            torch.nn.Sigmoid())
-
-"""
-Loss
-"""
-loss = torch.nn.CrossEntropyLoss()
-#loss= torch.nn.MSELoss()
+        Sample_num = x_train.shape[1]
+        Class_num = 2
+        Hiddenl = i
+        Hiddenl01 = j
 
 
+        """
+        Model
+        """
 
-"""
-Optimizer
-"""
-#optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+        model = torch.nn.Sequential(torch.nn.Linear(Sample_num, Hiddenl),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(Hiddenl, Hiddenl01),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(Hiddenl01, Class_num),
+                                    torch.nn.Sigmoid())
 
-"""
-Training
-"""
-
-epochs_num = 200
-sample_num_train = x_train.shape[0]
-sample_num_test = x_test.shape[0]
-sample_num_valid = x_valid.shape[0]
-
-
-for epoch in range(epochs_num):
-    optimizer.zero_grad()
-    yp = model(x_train)
-    train_loss = loss(yp, y_train.long())
-    train_acc = torch.sum(torch.max(yp, 1)[1] == y_train)
-    acc_train = (train_acc.float() / float(sample_num_train)) * 100
-
-    train_loss.backward()
-    optimizer.step()
-
-    yp_valid = model(x_valid)
-    yp_valid_acc = torch.sum(torch.max(yp_valid, 1)[1] == y_valid)
-    acc_valid = (yp_valid_acc.float() / float(sample_num_valid)) * 100
-
-    print('Epoch: ', epoch, 'loss: ', train_loss.item(), 'Train acc: ', acc_train.item(), 'Valid acc: ', acc_valid.item())
+        """
+        Loss
+        """
+        loss = torch.nn.CrossEntropyLoss()
+        #loss= torch.nn.MSELoss()
 
 
-print('END!')
+
+        """
+        Optimizer
+        """
+        #optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        optimizer = torch.optim.Adam(model.parameters(), lr=p)
+        p +=.2
+        """
+        Training
+        """
+
+        epochs_num = 200
+        sample_num_train = x_train.shape[0]
+        sample_num_test = x_test.shape[0]
+        sample_num_valid = x_valid.shape[0]
+
+
+        for epoch in range(epochs_num):
+            optimizer.zero_grad()
+            yp = model(x_train)
+            train_loss = loss(yp, y_train.long())
+            train_acc = torch.sum(torch.max(yp, 1)[1] == y_train)
+            acc_train = (train_acc.float() / float(sample_num_train)) * 100
+
+            train_loss.backward()
+            optimizer.step()
+
+            yp_valid = model(x_valid)
+            yp_valid_acc = torch.sum(torch.max(yp_valid, 1)[1] == y_valid)
+            acc_valid = (yp_valid_acc.float() / float(sample_num_valid)) * 100
+
+        print('Epoch: ', epoch, 'loss: ', train_loss.item(), 'Train acc: ', acc_train.item(), 'Valid acc: ', acc_valid.item(), i, j, p)
+
+
+
+        yp_test = model(x_test)
+        yp_test_acc = torch.sum(torch.max(yp_test, 1)[1] == y_test)
+        acc_test= (yp_test_acc.float() / float(sample_num_test)) * 100
+
+        print( 'test acc: ', acc_test.item())
+        d.append(acc_test.item())
+        print('END!')
+
+print(max(d))
